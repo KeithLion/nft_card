24b2c4ba142c54be951f907f7dce41ad9d98f368
KeithLion <nortonkiara6@gmail.com>
2022-07-08 20:38:31 -0700

    added icons and font

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import Paper from '@mui/material/Paper';
 import { shadows } from '@mui/system';
 // import { createTheme } from '@mui/system';
 // import { ThemeProvider } from '@emotion/react';
-import mainSquare from './images/image-equilibrium.jpg'
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
+import mainSquare from './images/image-equilibrium.jpg'
+import clock from './images/icon-clock.svg'
+import prism from './images/icon-ethereum.svg'
+import avatar from './images/image-avatar.png'
 
 
 
-
-function App() {
+function App(props) {
 
   // const theme = createTheme({
   //   palette: {
@@ -37,36 +39,44 @@ function App() {
   return (
     // <ThemeProvider theme={theme}>
 
-<Paper sx={{bgcolor:'#11255e', borderRadius: 0, padding: 25, width:'100%'}}>
-<Card sx={{border: 1, maxWidth: 308, position: 'relative'}} variant='card'>
+<Paper sx={{bgcolor:'#0c131c', borderRadius: 0, padding: 25, width:'100%', paddingBottom:0}}>
+<Card sx={{border: 1, maxWidth: 308, position: 'relative', bgcolor: '#141c28'}} variant='card'>
       <CardContent>
+        
         <Button style={{display:' block', position: 'relative'}}>
-
 <img src={mainSquare}  alt='' style={{maxWidth:'100%', borderRadius: 8, display:'block'}}/>
+
         </Button>
 
 
-<Link  href='#' sx={{textDecoration: 'none',':hover':{color:'black'}}} >
+<Link  href='#' sx={{textDecoration: 'none',color:'#ffffff',':hover':{color:'#007f7b'}, fontFamily:'outfit,sans-serif', fontWeight:600}} >
         Equilibrium #3429 
 </Link>
 
-      <p>
+      <p style={{color:'#577db2', fontFamily:'outfit,sans-serif', fontWeight:400}}>
         Our Equilibrium collection promotes balance and calm.
       </p>
       
 
 <div>
-        <Box sx={{display: 'flex',  justifyContent: 'space-between'}}>
-      <span>0.041 ETH </span> <span> 3 days left</span> 
+        <Box sx={{display: 'flex', justifyContent: 'space-between', fontFamily:'outfit,san-serif',fontWeight:400}}>
+      <span style={{color:'#007f7b'}}>
+        <img src={prism} alt=''/>
+        0.041 ETH 
+        </span> 
+      <span style={{color:'#577db2'}}> 
+<img src={clock} alt=''/>
+      3 days left
+      </span> 
         </Box>
 </div>
 
         <hr></hr>
-<div>
-        <span>
-
+<div style={{fontFamily:'outfit,sans-serif', fontWeight:300}}>
+        <span style={{color:'#577db2', display: 'inlineBlock'}}>
+        <img src={avatar} alt='' style={{maxWidth: 25}}/>
           Creation of </span> 
-        <Link href='#' sx={{textDecoration: 'none',':hover':{color:'black'}}}> Jules Wyvern </Link> 
+        <Link href='#' sx={{textDecoration: 'none',color:'#ffffff',':hover':{color:'#007f7b'}}}> Jules Wyvern </Link> 
 </div>
 
 
